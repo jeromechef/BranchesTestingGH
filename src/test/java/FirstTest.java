3c7271be93edcdd9882b7a1d72f3319cb3115cf1
michael.genenko <genenko@gmail.com>
2022-01-04 16:31:56 +0100

    Added on-demand static imports, removed unnecessary imports.

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,29 +1,25 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import java.nio.channels.Selector;
-
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class FirstTest {
     @Test
     void yandexSearchOpen() {
-        Selenide.open("https://yandex.ru");
-        $(Selectors.byText("Accept")).click();
+        open("https://yandex.ru");
+        $(byText("Accept")).click();
     }
 
     @Test
     void googleSearchOpen() {
-        Selenide.open("https://google.com");
-        $(Selectors.byText("I agree")).click();
+        open("https://google.com");
+        $(byText("I agree")).click();
     }
 
     @Test
     void bingSearchOpen() {
-        Selenide.open("https://bing.com");
-        $(Selectors.byText("Accept")).click();
+        open("https://bing.com");
+        $(byText("Accept")).click();
     }
 }
