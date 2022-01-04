ce950c5783c8bf6c5a4b96833b55c7145a7181ec
michael.genenko <genenko@gmail.com>
2022-01-04 16:21:22 +0100

    Added Google Search Open Page

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -9,4 +9,10 @@ import java.nio.channels.Selector;
 import static com.codeborne.selenide.Selenide.$;
 
 public class FirstTest {
+    @Test
+    void googleSearchOpen() {
+        Selenide.open("https://google.com");
+        $(Selectors.byText("I agree")).click();
+//        $(".FPdoLc").shouldHave(Condition.text("I'm Feeling Lucky!"));
+    }
 }
