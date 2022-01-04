d078b6d437645e6afa3045e3a06f940b91e7de51
michael.genenko <genenko@gmail.com>
2022-01-04 16:19:00 +0100

    Initial Commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.nio.channels.Selector;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class FirstTest {
+}
